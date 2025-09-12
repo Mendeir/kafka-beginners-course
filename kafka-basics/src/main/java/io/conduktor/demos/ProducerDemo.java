d10dcc49ef3aa2131b73167475be50661aff924a
Adrianne <adrianne.magracia@orangeandbronze.com>
2025-09-12 17:02:16 +0800

    Create first producer

--- kafka-basics/src/main/java/io/conduktor/demos/ProducerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/ProducerDemo.java
@@ -1,7 +1,30 @@
 package io.conduktor.demos;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Connection to localhost
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // Producer Properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // Creation of Producer
+        try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
+            producer.send(producerRecord);
+            producer.flush();
+        }
     }
 }
